9706e1a666063b4943275735e4b7d5a3370d097a
Chavez Harris <codedbychavez@gmail>
2022-06-20 21:44:03 -0300

    Added intial draft structure

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path');
+
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+// Pages
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/index.html'));
+})
+
+// Home
+app.get('/top-matches', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/top-matches.html'));
+})
+
+// Home
+app.get('/not-available', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/not-available.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`)
+})
