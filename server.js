85c67b97b04a5a6d495109996a54166f2b20cebc
Chavez Harris <codedbychavez@gmail>
2022-06-23 14:51:45 -0300

    Added ConfigCat to sample app

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const path = require('path');
 
+// Require the ConfigCat NodeJS client SDK package we've installed
+const configcat = require("configcat-node");
+
+// Initialize the client using your SDK key:
+let configCatClient = configcat.createClient("YOUR_CONFIGCAT_SDK_KEY");
+
 const app = express()
 const port = 3000
 
@@ -13,13 +19,17 @@ app.get('/', (req, res) => {
 })
 
 // Top Matches
-app.get('/top-matches', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages/top-matches.html'));
-})
+app.get('/top-matches', async(req, res) => {
 
-// Not Available
-app.get('/not-available', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages/not-available.html'));
+  // Create a variable to store the state of the feature flag from ConfigCat.
+  // This variable will be automatically updated every 60 seconds by default.
+  const canshowtopmatches = await configCatClient.getValueAsync("canshowtopmatches",  false);
+
+  if (canshowtopmatches) {
+    res.sendFile(path.join(__dirname, 'pages/top-matches.html'));
+  } else {
+    res.sendFile(path.join(__dirname, 'pages/not-available.html'));
+  }
 })
 
 app.listen(port, () => {
